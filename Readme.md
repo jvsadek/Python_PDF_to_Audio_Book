c05aa1cb792463d9ca3efb7657ddd46569dac3a0
jvsadek <j.v.sadek@gmail.com>
2024-01-28 22:10:07 +0400

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,22 @@
+# Simple Python PDF to Audio converter
+
+![Contributors](https://img.shields.io/github/contributors/jvsadek/TheMost_Dangerous_Writing_App_Flask?style=plastic)
+![Forks](https://img.shields.io/github/forks/jvsadek/TheMost_Dangerous_Writing_App_Flask)
+![Stars](https://img.shields.io/github/stars/jvsadek/TheMost_Dangerous_Writing_App_Flask)
+![Licence](https://img.shields.io/github/license/jvsadek/TheMost_Dangerous_Writing_App_Flask)
+![Issues](https://img.shields.io/github/issues/jvsadek/TheMost_Dangerous_Writing_App_Flask)
+
+## 
+### Description
+This is a simple python app to convert pdf file to audio book. It simply reads the pdf file using [pypdf](https://pypi.org/project/pypdf/) and use 
+[pyttsx3](https://pypi.org/project/pyttsx3/) to convert text to speech and combine using [pydub](https://github.com/jiaaro/pydub) to concat it to one file.
+
+### Project motivation
+I completed this portfolio website, as one of the assignments in professional [Python 100 Days bootcamp](https://www.udemy.com/course/100-days-of-code/) by [Dr. Angela Yu](https://github.com/angelabauer).
+
+### Quick Start
+- Install the requirements in requirements.txt
+- Update the pdf file location
+- Run it in python 
+
+[//]: # (- Dynamic version at [Render hosted]&#40;https://portfolio-website-d1x4.onrender.com&#41;)
---
+++ main.py
@@ -0,0 +1,53 @@
+import pyttsx3
+from pypdf import PdfReader
+from pydub import AudioSegment
+from pathlib import Path
+import os
+
+file = open("Resume-Joseph-Sadek_2024.pdf", 'rb')
+# file = open("demo.pdf", 'rb')
+pdf_reader = PdfReader(file)
+num_pages = len(pdf_reader.pages)
+print(num_pages)
+
+# Initalization of the Pyttsx3 engine
+engine = pyttsx3.init()
+
+#Set the prefered voice and rate
+voices = engine.getProperty('voices')
+rate = engine.getProperty('rate')
+engine.setProperty('voice', voices[7].id)
+engine.setProperty('rate', rate-5)
+data = []
+for num in range(0, num_pages):
+    page = pdf_reader.pages[num]
+    data.append(page.extract_text())
+    # play.say('This is the converstion of pdf file to audio')
+    # engine.say(data[num])
+    engine.save_to_file(data[num], f"./audio_segments/{file.name[:-4]}_{num + 1}.mp3")
+    # play.save_to_file('Hello World', 'test.mp3')
+    engine.runAndWait()
+
+#Saving each page to a different audio segment
+p = Path('./audio_segments')
+
+#Importing the separate Audio files from audio_segements
+audio_files = [mp3_file for mp3_file in p.glob(f"{file.name[:-4]}*.mp3")]
+#Sorting the audio files with creation date to avoid mis match
+audio_files.sort(key=os.path.getmtime)
+# audio_segments = [AudioSegment.from_file(mp3_file) for mp3_file in p.glob(f"{file.name[:-4]}*.mp3")]
+audio_segments = [AudioSegment.from_file(audio_file) for audio_file in audio_files]
+first_seg = audio_segments.pop(0)
+
+#Appending different audio segments into one file
+playlist = first_seg
+for seg in audio_segments:
+
+    playlist = playlist.append(seg)
+    playlist_length = len(playlist) / (1000 * 60)
+
+# lets save it with the length of file in minutes!
+with open(f"{file.name[:-4]}.mp3", 'wb') as out_f:
+    playlist.export(out_f, format='mp3')
+
+
